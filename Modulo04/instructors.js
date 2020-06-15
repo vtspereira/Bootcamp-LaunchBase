eb589fb083e9f084c788ba9179b20e90a8658cc4
Vtspereira <vtspereira@hotmail.com>
2020-06-15 16:54:46 -0300

    Configuration form

--- Modulo04/instructors.js
+++ Modulo04/instructors.js
@@ -1,59 +1,44 @@
 const fs = require('fs')
 const data = require('./data.json')
-const { age } = require('./utils')
+const { age, date } = require('./utils')
 
-exports.show = function(req ,res){
+exports.show = function (req, res) {
 
     const { id } = req.params
 
-    const foundInstructor = data.instructors.find(function(instructor){
+    const foundInstructor = data.instructors.find(function (instructor) {
         return instructor.id == id
     })
 
-    if(!foundInstructor) return res.send("Instructor not found!!")
+    if (!foundInstructor) return res.send("Instructor not found!!")
 
-    function age(timestamp){
-        const today = new Date()
-        const birthDate = new Date(timestamp)
-
-        let age = today.getFullYear() - birthDate.getFullYear()
-        const month = today.getMonth - birthDate.getMonth()
-
-        if(month < 0 ||
-            month == 0 && 
-            today.getDate() <= birthDate.getDate()){
-            age = age - 1
-        }
-
-        return age
-    }
 
     const instructor = {
         ...foundInstructor,
         age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at: "",
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
     }
 
     return res.render("instructors/show", { instructor })
 }
 
-exports.post = function(req, res){
+exports.post = function (req, res) {
 
     const keys = Object.keys(req.body)
 
-    for(key of keys) {
-       if(req.body[key] == "")
-       return res.send('Please, fill all fields!')
+    for (key of keys) {
+        if (req.body[key] == "")
+            return res.send('Please, fill all fields!')
     }
 
-    let {  avatar_url, name, birth, gender, services } = req.body
+    let { avatar_url, name, birth, gender, services } = req.body
 
     birth = Date.parse(birth)
     const created_at = Date.now()
     const id = Number(data.instructors.length + 1)
 
-    data.instructors.push({ 
+    data.instructors.push({
         id,
         avatar_url,
         name,
@@ -63,12 +48,58 @@ exports.post = function(req, res){
         services
     })
 
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
         if (err) return res.send("Write file error!")
 
-        return res.redirect("/instructors")
+        return res.redirect(`/instructors/${id}`)
+    })
+    // return res.send(req.body)
+}
+
+exports.edit = function (req, res) {
+
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function (instructor) {
+        return instructor.id == id
     })
 
+    if (!foundInstructor) return res.send("Instructor not found!!")
 
-    // return res.send(req.body)
+    const instructor ={
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', { instructor  })
+}
+
+exports.put = function (req, res) {
+
+    const { id } = req.body
+
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function (instructor, foundIndex) {
+        if(id == instructor.id){
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!!")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("Write error!")
+
+        return res.redirect(`/instructors/${id}`)
+    })
 }
