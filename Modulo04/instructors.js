415e53ba609c8bee23dbde4fd6516787fd66ab53
Vtspereira <vtspereira@hotmail.com>
2020-06-16 23:49:30 -0300

    Listing instructors in the index

--- Modulo04/instructors.js
+++ Modulo04/instructors.js
@@ -2,6 +2,10 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./utils')
 
+exports.index =  function(req, res){
+    return res.render("instructors/index", { instructors: data.instructors })
+}
+
 exports.show = function (req, res) {
 
     const { id } = req.params
@@ -101,5 +105,22 @@ exports.put = function (req, res) {
         if(err) return res.send("Write error!")
 
         return res.redirect(`/instructors/${id}`)
+    })
+}
+
+exports.delete = function (req, res) {
+
+    const { id } = req.body
+
+    const filteredInstructors = data.instructors.filter(function(instructor){
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send("Write file error!")
+
+        return res.redirect("/instructors")
     })
 }
