cb57a201b2d74ade4906034920457aaf30b5b456
Vtspereira <vtspereira@hotmail.com>
2020-06-20 23:52:38 -0300

    Studying about databases

---
+++ Modulo05/public/script.js
@@ -0,0 +1,17 @@
+const currentPage = location.pathname
+const menuItems = document.querySelectorAll("header .links a")
+
+for (item of menuItems) {
+    if (currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add("active")
+    }
+}
+
+
+const formDelete = document.querySelector("#form-delete")
+formDelete.addEventListener("submit", function (event) {
+    const confirmation = confirm("Deseja deletar?")
+    if (!confirmation) {
+        event.preventDefault()
+    }
+})
