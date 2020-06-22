b22f6740a5e1cee0cc2c2c71796ab2482a7253f0
Vtspereira <vtspereira@hotmail.com>
2020-06-22 19:41:55 -0300

    Ending from the instructors database

--- Modulo05/src/app/controllers/instructors.js
+++ Modulo05/src/app/controllers/instructors.js
@@ -1,14 +1,12 @@
+const Instructor = require('../models/Instructor')
 const { age, date } = require('../../lib/utils')
-const db = require('../../config/db')
 
 module.exports = {
 
     index(req, res) {
 
-        db.query(`SELECT * FROM instructors`, function(err, results){
-            if(err) return res.send("Database Error!")
-
-            return res.render("instructors/index", { instructors: results.rows })
+        Instructor.all(function(instructors) {
+            return res.render("instructors/index", { instructors })
         })
 
     },
@@ -26,42 +24,34 @@ module.exports = {
                 return res.send('Please, fill all fields!')
         }
 
-        const query = `
-            INSERT INTO instructors (
-                name,
-                avatar_url,
-                gender,
-                services, 
-                birth,
-                created_at
-            ) VALUES ($1, $2, $3, $4, $5, $6)
-            RETURNING id 
-        `
-            const values = [
-                req.body.name,
-                req.body.avatar_url,
-                req.body.gender,
-                req.body.services,
-                date(req.body.birth).iso,
-                date(Date.now()).iso
-            ]
-
-       db.query(query, values, function(err, results){
-           if(err) return res.send("Database Error!")
-
-           return res.redirect(`/instructors/${results.rows[0].id}`)
-       })
+        Instructor.create(req.body, function(instructor) {
+            return res.redirect(`/instructors/${instructor.id}`)
+        })
 
     },
     show(req, res) {
 
-        return
+        Instructor.find(req.params.id, function(instructor){
+            if(!instructor) return res.send("Instructor not found!")
+
+            instructor.age = age(instructor.birth)
+            instructor.services = instructor.services.split(",")
+
+            instructor.created_at = date(instructor.created_at).format
+
+            return res.render("instructors/show", { instructor })
+        })
 
     },
     edit(req, res) {
 
-        return
+        Instructor.find(req.params.id, function(instructor){
+            if(!instructor) return res.send("Instructor not found!")
 
+            instructor.birth = date(instructor.birth).iso
+
+            return res.render("instructors/edit", { instructor })
+        })   
 
     },
     put(req, res) {
@@ -73,12 +63,16 @@ module.exports = {
                 return res.send('Please, fill all fields!')
         }
 
-        return
+        Instructor.update(req.body, function() {
+            return res.redirect(`/instructors/${req.body.id}`)
+        })
 
     },
     delete(req, res) {
 
-        return
+        Instructor.delete(req.body.id, function() {
+            return res.redirect(`/instructors`)
+        })
 
     },
 
