e6eecf6f15002ee78bd6e71de74ac5dd820df8e7
Vtspereira <vtspereira@hotmail.com>
2020-06-26 22:49:12 -0300

    Instructor filter added

--- Modulo05/src/app/controllers/instructors.js
+++ Modulo05/src/app/controllers/instructors.js
@@ -6,10 +6,17 @@ module.exports = {
 
     index(req, res) {
 
-        Instructor.all(function(instructors) {
-            return res.render("instructors/index", { instructors })
-        })
+        const { filter } = req.query
 
+        if( filter ) {
+            Instructor.findBy(filter, function(instructors) {
+                    return res.render("instructors/index", { instructors, filter })
+            })
+        } else {
+            Instructor.all(function(instructors) {
+                return res.render("instructors/index", { instructors })
+            })   
+        }
     },
     create(req, res) {
 
